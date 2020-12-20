f0b9ca53cc48d2258069845af6d90eb3c8b4ca8b
taiyaki <60641436+taiyaki-ichi@users.noreply.github.com>
2020-12-20 14:15:03 +0900

    window.hpp

--- src_test/main.cpp
+++ src_test/main.cpp
@@ -1,6 +1,12 @@
+#include"window.hpp"
 
 int main()
 {
+
+	auto hwnd = graphics::create_window(L"aaaaa", 400.f, 400.f);
+	while (graphics::update_window()) {
+
+	}
 
 	return 0;
 }
---
+++ src_test/window.hpp
@@ -0,0 +1,75 @@
+#pragma once
+#include<Windows.h>
+#include<optional>
+
+namespace graphics
+{
+
+	namespace {
+
+		LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
+		{
+			if (msg == WM_DESTROY) {
+				PostQuitMessage(0);
+				return 0;
+			}
+			return DefWindowProc(hWnd, msg, wParam, lParam);
+		}
+	}
+
+	//ウィンドウの作製
+	HWND create_window(const wchar_t* window_name, float width, float height)
+	{
+		HINSTANCE hInstance = GetModuleHandle(nullptr);
+
+		//ウィンドウクラス設定
+		WNDCLASSEX wcex{};
+		wcex.cbSize = sizeof(WNDCLASSEX);
+		wcex.lpfnWndProc = WndProc;
+		wcex.lpszClassName = window_name;
+		wcex.hInstance = hInstance;
+
+		//ウィンドウクラスの登録
+		RegisterClassEx(&wcex);
+
+		//ウィンドウサイズの設定
+		RECT rect{ 0,0,static_cast<LONG>(width),static_cast<LONG>(height) };
+		AdjustWindowRect(&rect, WS_OVERLAPPEDWINDOW, false);
+
+		HWND hwnd = CreateWindow(
+			wcex.lpszClassName,			//クラス名
+			window_name,				//タイトルバー
+			WS_OVERLAPPEDWINDOW,		//タイトルバーと境界線があるウィンドウ
+			CW_USEDEFAULT,				//x座標
+			CW_USEDEFAULT,				//y座標
+			rect.right - rect.left,		//ヨコ
+			rect.bottom - rect.top,		//タテ
+			nullptr,					//親ウィンドウ
+			nullptr,					//メニューハンドル
+			wcex.hInstance,				//呼び出しアプリケーションハンドル
+			nullptr						//追加パラメータ
+		);
+
+		//ウィンドウの表示
+		ShowWindow(hwnd, SW_SHOW);
+
+		return hwnd;
+	}
+
+	//メッセージの処理
+	bool update_window()
+	{
+		MSG msg;
+		if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+			DispatchMessage(&msg);
+
+		//終了のメッセージならばfalse
+		if (msg.message == WM_QUIT)
+			return false;
+		else
+			return true;
+	}
+
+
+
+}
