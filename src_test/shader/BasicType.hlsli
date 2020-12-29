606fe46bb6e4786a9f007070477974f9729be6dc
taiyaki <60641436+taiyaki-ichi@users.noreply.github.com>
2020-12-29 21:39:19 +0900

    src_test削除

--- src_test/shader/BasicType.hlsli
+++
@@ -1,4 +0,0 @@
-struct BasicType {
-	float4 svpos:SV_POSITION;//システム用頂点座標
-	float2 uv:TEXCOORD;//UV値
-};
--- src_test/shader/PixelShader1.hlsl
+++
@@ -1,9 +0,0 @@
-#include"BasicType.hlsli"
-Texture2D<float4> tex:register(t0);//0番スロットに設定されたテクスチャ
-SamplerState smp:register(s0);//0番スロットに設定されたサンプラ
-
-//ピクセルシェーダ
-float4 main(BasicType input) : SV_TARGET{
-	return float4(tex.Sample(smp,input.uv));
-	//return float4(1,0,0,1);
-}
